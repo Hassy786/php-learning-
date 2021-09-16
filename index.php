b0a90807a5f6c82263fca0c1a0277195678167b9
Hassy786 <90459106+Hassy786@users.noreply.github.com>
2021-09-16 19:15:15 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,169 @@
+<html>
+<head>
+    <title>php code </title>
+</head>
+<link rel="stylesheet" href="index.css">
+<body>
+
+<h1><?php echo "Learning PHP from NINJA" ?></h1>
+
+
+<div> 
+    <?php
+
+//  constant variables in php 
+    // define('Name','Mak');
+    // echo NAme
+//echo "hello world from hasy";  
+?>
+</div>
+    <?php
+// echo "Hello world";
+    //  $num1 = 10;
+    //  $num2 = 10;
+
+    //  $sum = $num1 + $num2;
+    // echo "the sum of num1 and num2 is :, $sum" 
+
+    // $name = "Hassy";
+    // echo "Hello ," . $name
+
+    // $greeting = "Good Morning";
+    // $greeting_message = "Have a nice day!";
+
+    // echo  $greeting . $greeting_message;
+    // echo "<br>";
+    
+    //program to check student grade based on the marks
+
+    // $marks = 45;
+
+    // if($marks >= 60)
+    // {
+    //     $grade = "First Division";
+        
+    // }
+    // elseif ($marks >=45 ){
+    //     $grade = "Second Division";
+    // }
+    // elseif($marks >=33)
+    // {
+    //     $grade = "Third Divison";
+    // }
+    // else
+    // {
+    //     $grade = "fail";
+
+    // }
+    // echo "The final grade is : " .$grade
+
+
+    // loop in the php 
+    
+    // $num =3;
+    // $factorial = 1;
+    // for($i = $num; $i >=1; $i--)
+    // {
+    //     $factorial = $factorial * $i;
+    // }
+
+    //     echo "the factorail of number 3 is " . $factorial;
+    // ?>
+
+
+
+     <?php
+// arrrays in php would be index and associative 
+
+// IndexedArrays
+
+$studentNames =array('Don','MAK','Been','Mickel','Hassy');
+
+// array_push($studentNames, 'David');
+// printing the values of array
+
+// print_r($studentNames);
+
+// array_pop($studentNames);
+//print_r($studentNames);
+
+//Assocaiative arrays 
+
+// $array1 = array('key1' => '1', 'key2' => '2' , 'key3' => '3');
+
+
+//    // echo $array1['key1'];
+//     print_r($array1);
+
+
+
+// multi dimensional arrays
+
+// $blogs =[
+// ['name'=> 'hassy', 'content'=> 'hello world', 'like'=>    10],
+// ['name'=> 'mak',   'content'=>  'hello boies',  'like'=>  50],
+// ['name'=> 'donJoe','content'=> 'hello boies', 'like'=>    10],
+
+// ];
+
+// //how to add in the blogs array 
+
+// $blogs []= ['name'=> 'MJK', 'content'=> 'sdasdsadsa', 'likes'=> 20];
+
+// // print_r($blogs[0]['content']);
+
+//  echo $blogs[1]['name'];
+// print_r($blogs);
+// //echo count($blogs);
+
+
+
+// for loop
+
+// for($i=0; $i< count($studentNames); $i++){
+    
+//     echo $studentNames[$i]. '<br />';
+// }
+
+// foreach($studentNames as $name){
+//     echo $name . '<br />';
+// }
+  
+// For input
+// Hello World
+// $a = readline('Enter a string: ');
+  
+// For output
+
+// echo $a;    
+
+
+// Input section
+// $a = 10
+// $a = (int)readline('Enter an integer: ');
+  
+// $b = 9.78
+// $b = (float)readline('Enter a floating'
+            // . ' point number: ');
+  
+// Entered integer is 10 and
+// entered float is 9.78
+// echo "Entered integer is " . $a 
+    // . " and entered float is " . $b;
+
+
+
+
+//    echo 5 < 10;
+    //echo 5 >10 ;
+    // 5 == 10  // loose comparison
+    //  5 != 10;
+
+     echo 'abc' > 'eass';
+
+?>
+
+ 
+
+</body>
+</html>
