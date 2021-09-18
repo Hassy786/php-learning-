1d84f758a359f128b6643f0e6f19a291aa649ed6
Hassy786 <90459106+Hassy786@users.noreply.github.com>
2021-09-18 20:24:49 +0500

    Add files via upload

---
+++ pizzza's project php/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+
+    <!-- each and everything is on the single page by using the include --> 
+    <?php include('templates/header.php'); ?>
+    <?php include('templates/footer.php'); ?>
+    
+
+</html>
---
+++ pizzza's project php/templates/footer.php
@@ -0,0 +1,4 @@
+
+<footer class ="section"></footer> 
+<div class="center grey-text">Copyright by Pizzas  </div>
+</body>
