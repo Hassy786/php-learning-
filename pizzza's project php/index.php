b7c0d3bd31051ae4385c33924818f105c86e31b1
Hassy786 <90459106+Hassy786@users.noreply.github.com>
2021-09-19 21:16:03 +0500

    updated

--- pizzza's project php/index.php
+++ pizzza's project php/index.php
@@ -16,7 +16,7 @@
         // how i getting the data from the DB
 
         // query from the pizza database 
-        $sql = ' SELECT title,	ingredients, id FROM  pizzzzas ';
+        $sql = ' SELECT title,	ingredients FROM  pizzzzas order by created_at ';
 
         // make query and getting a result 
         $result = mysqli_query($conn, $sql);
@@ -24,12 +24,46 @@
         // fetch the rows in the array 
         $pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-        print_r($pizzas);
+        // free result from memory
+        mysqli_free_result($result);
+
+        //close the connection always 
+            mysqli_close($conn);    
+
+        // $check = explode(',', $pizzas[0]['ingredients']);
+        // print_r($check);
 
         ?>
    
     <!-- shows the header -->
     <?php include('templates/header.php'); ?>
+    
+    <h4 class="center grey-text">Pizzas!</h4>
+    <div class="container">
+        <div class="row">
+        <?php foreach($pizzas as $pizza) {?>
+            <div class="col s6 md3">
+                <div class="card z-depth-0">
+                <div class="card-content center">
+                    <h6><?php  echo htmlspecialchars($pizza['title'])?></h6>
+                    <ul>
+                        <?php  foreach(explode(',', $pizza['ingredients']) as $ing) {?>
+                            <li><?php echo htmlspecialchars($ing) ?></li>
+                            <?php }?>
+                    </ul>
+                </div>
+                <div class="card-action right-align">
+                    <a href="#" class="brand-text">more info </a>
+                </div>
+
+                </div>
+            </div>
+
+    
+    <?php  }   ?>
+
+    </div>
+    </div>
    
     <!-- shows the footer -->
     <?php include('templates/footer.php'); ?>
