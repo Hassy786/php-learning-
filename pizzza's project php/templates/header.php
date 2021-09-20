b6253c88ff2ee0d527bfd3ea2497d02f2f0f8c1e
Hassy786 <90459106+Hassy786@users.noreply.github.com>
2021-09-20 19:36:03 +0500

    updated

--- pizzza's project php/templates/header.php
+++ pizzza's project php/templates/header.php
@@ -7,12 +7,28 @@
        <!-- Compiled and minified CSS -->
        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 </head>
+<link rel="stylesheet" href="templates/index.css">
 <body class ="grey lighten-4">
+
+    <style>
+
+.pizza {
+    border-style: none;
+    width: 100px;
+    margin: 40px auto -30px;
+    display: block;
+    position: relative;
+    top: -30px;
+
+    </style>
+
+
+
     <nav class="white z-depth-0">  <!-- this is a navbar of the page --> 
         <div class= "container">
-            <a href="#" class= "brand-logo brand-text">Pizzza</a> <!--- this is not the materilize class -->
+            <a href="index.php" class= "brand-logo brand-text">Pizzza</a> <!--- this is not the materilize class -->
             <ul id="nav-mobile" class ="right hide-on-small-and-down"> 
-                <li><a href="#" class="btn brand z-depth-0">Add Pizaa</a></li>
+                <li><a href="add.php" class="btn brand z-depth-0">Add Pizaa</a></li>
                 </ul>
 
         </div>
