42764932fbf8993b10202e73318c6e6c03084d36
Hassy786 <90459106+Hassy786@users.noreply.github.com>
2021-09-20 19:34:05 +0500

    Add files via upload

---
+++ ternary.php
@@ -0,0 +1,19 @@
+<?php 
+
+$marks  = 90;
+
+// if($marks > 100){
+//     echo 'high marks';
+
+// }else {
+//     echo 'less marks';
+
+// }
+
+
+// ternary 
+
+echo $marks > 50 ? 'passed': 'then less marks to passs :(';
+ 
+
+?>
