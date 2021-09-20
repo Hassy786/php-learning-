cf6ef07bd237835a50ba072db8f5c8f4888b1676
Hassy786 <90459106+Hassy786@users.noreply.github.com>
2021-09-20 18:25:22 +0500

    delete button updated

--- pizzza's project php/details.php
+++ pizzza's project php/details.php
@@ -1,7 +1,22 @@
 <?php 
 
     include('config\db_connect.php');
+	// simple step to make POST request for the delete the pizza records
+	if(isset($_POST['delete'])){
+		$id_to_delete = mysqli_real_escape_string($conn, $_POST,['id_to_delete']);
 
+		//mysqll simple query for deleting the records from db 
+		$sql = "DELETE FROM pizzzzas WHERE id =$id_to_delete"
+
+		if(mysqli_query($conn, $sql)){
+			header('Location: index.php');
+
+		}else {
+			echo 'query errorr' . mysqli_error($conn);
+		}
+
+
+	}
 
     // check GET request id param
 	if(isset($_GET['id'])){
@@ -32,7 +47,17 @@
 			<p>Created by <?php echo $pizza['email']; ?></p>
 			<p><?php echo date($pizza['created_at']); ?></p>
 			<h5>Ingredients:</h5>
-			<p><?php echo $pizza['ingredients']; ?></p>
+			<p><?php echo htmlspecialchars($pizza['ingredients']); ?></p>
+			
+			<form action="details.php" method= "POST">
+			<input type = "hidden" name ="id_to_delete" value="<?php echo $pizza['id'] ?>">
+			<input type="submit" name="delete" value="Delete" class="btn brand z-depth-0">
+		
+		
+			</form>
+
+
+
 		<?php else: ?>
 			<h5>No such pizza exists.</h5>
 		<?php endif ?>
